1fe7779c31a639fae24df32c3eb732da2ca21ab6
wojtekno <wjw.nowak@gmail.com>
2017-10-30 13:15:45 +0100

    Add MineSweeper.interface

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		MineSweeper saper = new MineSweeperImplementation();
 
 	}
 
---
+++ src/MineSweeperImplementation.java
@@ -0,0 +1,16 @@
+
+public class MineSweeperImplementation implements MineSweeper {
+
+	@Override
+	public void setMineField(String mineField) throws IllegalArgumentException {
+		
+		
+	}
+
+	@Override
+	public String getHintField() throws IllegalStateException {
+		
+		return null;
+	}
+
+}
